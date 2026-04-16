dd5470146e1468b64d636d21d21a318da77666af
Krishna <krizzz7564@gmail.com>
2026-04-16 23:51:50 +0530

    UC1: Application Entry & Welcome Message

---
+++ App/src/UseCase1HotelBookingApp.java
@@ -0,0 +1,17 @@
+/**
+ * UseCase1HotelBookingApp
+ * Entry point of the Hotel Booking System
+ * @author Krishna
+ * @version 1.0
+ */
+public class UseCase1HotelBookingApp{
+
+    public static void main(String[] args) {
+
+        System.out.println("====================================");
+        System.out.println("   Welcome to Book My Stay App");
+        System.out.println("   Hotel Booking System v1.0");
+        System.out.println("====================================");
+
+    }
+}
